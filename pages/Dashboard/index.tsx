952d841d223b69483a099d5ab8b49fb5162d5fda
rafaazvd <rafa-spdf@hotmail.com>
2021-04-22 17:05:42 -0300

    fix dash

--- pages/Dashboard/index.tsx
+++ pages/Dashboard/index.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { GrLinkedinOption } from 'react-icons/gr';
-import { FiFacebook, FiInstagram } from 'react-icons/fi';
 
 import {
   Container,
@@ -9,6 +7,24 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 const Dashboard: React.FC = () => {
+
+  function checkDevice() {
+    if (navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)
+    ) {
+      return true; // está utilizando celular
+    }
+
+    return false; // não é celular
+  }
+  if (!checkDevice()) {
+    alert('Nao temos ainda uma versao mobile disponivel, acesse pelo navegador de seu computador para uma melhor experiencia');
+  }
   return (
     <Container>
       <Header />
--- pages/components/Header/index.tsx
+++ pages/components/Header/index.tsx
@@ -1,34 +1,12 @@
 import React from 'react'
-import { GrLinkedinOption } from 'react-icons/gr'
-import { FiFacebook, FiInstagram } from 'react-icons/fi'
 
 import {
   Container,
   LogoImg,
-  Ptbr,
-  AreaStore,
-  AreaEnrollment,
-  RightComponent,
 } from './styles';
 
 const Dashboard: React.FC = () => {
-  function checkDevice() {
-    if (navigator.userAgent.match(/Android/i)
-    || navigator.userAgent.match(/webOS/i)
-    || navigator.userAgent.match(/iPhone/i)
-    || navigator.userAgent.match(/iPad/i)
-    || navigator.userAgent.match(/iPod/i)
-    || navigator.userAgent.match(/BlackBerry/i)
-    || navigator.userAgent.match(/Windows Phone/i)
-    ) {
-      return true; // está utilizando celular
-    }
-
-    return false; // não é celular
-  }
-  if (checkDevice()) {
-    alert('Nao temos ainda uma versao mobile disponivel, acesse pelo navegador de seu computador para uma melhor experiencia');
-  }
+  
   return (
     <Container>
       <LogoImg src="/raio.png" />

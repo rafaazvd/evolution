ed6f16ed66ee833485cbd0656c32cc3065eddf5b
rafaazvd <rafa-spdf@hotmail.com>
2021-04-22 17:24:54 -0300

    fix dash

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { AppProps } from 'next/app'
 import GlogalStyles from '../styles/global'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const navigator = {};
   return (
     <>
      <Component {...pageProps} />
